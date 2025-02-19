37c2ac38786ec5e6b329e0718fb1dca8935e6c81
sidor <6hr@de6U>
2025-02-19 12:14:05 +0300

    024 Parametrized tests

--- src/test/java/tests/Player.java
+++ src/test/java/tests/Player.java
@@ -1,4 +1,30 @@
 package tests;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
+
 public class Player {
+
+    @ParameterizedTest
+    @CsvSource({"smolov,forward,RU", "ngamale,forward,CAM", "shunin,goalkeeper,RU"})
+    public void checkingForRussianPlayers(String name, String role, String country) {
+        Assertions.assertTrue(country.contains("RU"));
+    }
+
+
+    @Test
+    public void sum1() {
+        int a = 3;
+        int b = 10;
+        Assertions.assertTrue(a<b);
+    }
+
+    @Test
+    public void sum2() {
+        int a = 13;
+        int b = 1110;
+        Assertions.assertTrue(a>b);
+    }
 }
